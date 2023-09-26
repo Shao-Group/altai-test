5abf66cde8cfe0fa97ec7df01ee77c52bbbde93c
Carl Zang <xbz5174@psu.edu>
2023-09-26 13:32:13 -0400

    simulation alignment

--- sim-scripts/run_simulation.sh
+++ sim-scripts/run_simulation.sh
@@ -63,3 +63,34 @@ do
 	mv "tmp".dm6.polyester.$j".txabd.tsv" "tmp".dm6.polyester.$j".no0tx.fa" tmp
 done
 
+
+
+
+
+################################ preparations for assembly: index and alignment ###############
+vcf="dm6.intersect.selected.gt.vcf"
+genome="dm6_paternal_genome.fa"
+
+# star index, align, sort
+mkdir index-star
+sh ../sim-scripts/do_index.sh $genome index-star
+
+for i in $(ls | grep sim)
+do
+	cd $i
+	echo "current dir is" $i ":="  $(pwd)
+	
+	mkdir aligned_star
+	for sample_id in sample_01 sample_02
+	do
+		output_prefix="aligned_star/"$sample_id
+		echo "start to align sample" $sample_id
+		sh ../../sim-scripts/do_star.sh ../index-star $sample_id"_1.fasta" $sample_id"_2.fasta" ../$vcf $output_prefix
+		echo "finished align sample" $sample_id
+	done
+	
+	cd ..
+done
+
+
+

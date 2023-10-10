c69381f74cdf32e2f2cc3aad693469f421f9c4bd
Carl Zang <xbz5174@psu.edu>
2023-10-10 15:20:54 -0400

    assembly + quant

--- sim-scripts/do_assembly_quant.sh
+++ sim-scripts/do_assembly_quant.sh
@@ -22,22 +22,50 @@ then
 	scallop2 -i $bam \
 		 -o scallop2.$4.gtf \
 		 --library_type $library_type \
-         	--verbose 2
+         	 --verbose 2 \
+		 1> scallop2.$4.log 2> scallop2.$4.err
 else
 	echo scallop2 assembly skipped for $1
 fi
 
 mkdir assembly_quant
 
+# make merged-alleles transcripts fasta
 gffread -w assembly_quant/scallop2.$4.allele1.fa -g $pat_genome scallop2.$4.gtf
 gffread -w assembly_quant/scallop2.$4.allele2.fa -g $mat_genome scallop2.$4.gtf
-
-kallisto index      -i assembly_quant/scallop2.$4.allele1.index    assembly_quant/scallop2.$4.allele1.fa
-kallisto quant -t 8 -i assembly_quant/scallop2.$4.allele1.index -o assembly_quant/scallop2.$4.allele1.quant $library_kallisto $left $right
-
-kallisto index 	    -i assembly_quant/scallop2.$4.allele2.index    assembly_quant/scallop2.$4.allele2.fa
-kallisto quant -t 8 -i assembly_quant/scallop2.$4.allele2.index -o assembly_quant/scallop2.$4.allele2.quant $library_kallisto $left $right
+sed -i 's/^>/>pat_/' assembly_quant/scallop2.$4.allele1.fa
+sed -i 's/^>/>mat_/' assembly_quant/scallop2.$4.allele2.fa
+cat assembly_quant/scallop2.$4.allele1.fa assembly_quant/scallop2.$4.allele2.fa > assembly_quant/scallop2.$4.merged.fa
 
 
-# grab by non zero
+# kallisto quant on merged sc2 outptus
+kallisto index      -i assembly_quant/scallop2.$4.index    assembly_quant/scallop2.$4.merged.fa \
+	1> assembly_quant/kallisto_index.$4.log 2> assembly_quant/kallisto_index.$4.err
+kallisto quant -t 8 -i assembly_quant/scallop2.$4.index -o assembly_quant/scallop2.$4.quant $library_kallisto $left $right \
+	1> assembly_quant/kallisto_quant.$4.log 2> assembly_quant/kallisto_quant.$4.err
+
+# grab by non zero: skip first line, estimate count > 0, 
+as1_id="assembly_quant/scallop2.$4.quant/expressed.transcripts.ale1.id"
+as2_id="assembly_quant/scallop2.$4.quant/expressed.transcripts.ale2.id"
+as1_spec_id="assembly_quant/scallop2.$4.quant/expressed.transcripts.ale1spec.id"
+as2_spec_id="assembly_quant/scallop2.$4.quant/expressed.transcripts.ale2spec.id"
+nonspec_id="assembly_quant/scallop2.$4.quant/expressed.transcripts.nonspec.id"
+merged_id="assembly_quant/scallop2.$4.quant/expressed.transcripts.merged.id"
+
+tail -n +2 assembly_quant/scallop2.$4.quant/abundance.tsv | awk '{if ($4 > 0) {print $1;}}' > assembly_quant/scallop2.$4.quant/expressed.transcripts.id
+cat assembly_quant/scallop2.$4.quant/expressed.transcripts.id | grep "^>pat_" | sed 's/^>pat_/>/' | sort > $as1_id
+cat assembly_quant/scallop2.$4.quant/expressed.transcripts.id | grep "^>mat_" | sed 's/^>mat_/>/' | sort > $as2_id
+comm -12 $as1_id $as2_id > $merged_id
+comm -13 $as1_id $as2_id > $as2_spec_id
+comm -23 $as1_id $as2_id > $as1_spec_id
+cat $as1_id $as2_id | sort | uniq > $nonspec_id
+
+gffread -T --ids $as1_id      -o scallop2.$4.allele1.gtf     scallop2.$4.gtf
+gffread -T --ids $as2_id      -o scallop2.$4.allele2.gtf     scallop2.$4.gtf
+gffread -T --ids $as1_spec_id -o scallop2.$4.allele1spec.gtf scallop2.$4.gtf
+gffread -T --ids $as2_spec_id -o scallop2.$4.allele2spec.gtf scallop2.$4.gtf
+gffread -T --ids $nonspec_id  -o scallop2.$4.nonspec.gtf     scallop2.$4.gtf
+gffread -T --ids $merged_id   -o scallop2.$4.merged.gtf      scallop2.$4.gtf
+
+
 
